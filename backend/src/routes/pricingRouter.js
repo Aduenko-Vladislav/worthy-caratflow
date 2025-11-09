15c64f6a979f8c078e27707436b67a3087e52fa7
VladislavAduenko <adyenko.job@gmail.com>
2025-11-09 17:55:37 +0200

    fix: add logs, fix endpoint url

--- backend/src/routes/pricingRouter.js
+++ backend/src/routes/pricingRouter.js
@@ -2,10 +2,11 @@ import express from "express";
 import { validator } from "../middleware/validation.js";
 import { schemaPrice } from "../validation/pricingSchema.js";
 import { PricingService } from "../service/pricingService.js";
+import logger from "../logger/winstonLogging.js";
 
 const pricingRouter = express.Router();
 
-pricingRouter.post("/price", validator(schemaPrice), (req, res) => {
+pricingRouter.post("/calculate", validator(schemaPrice), (req, res) => {
   const { shape, carat, color, clarity, polish, symmetry, fluorescence } =
     req.body;
   const price = PricingService.calculatePrice({
@@ -17,6 +18,7 @@ pricingRouter.post("/price", validator(schemaPrice), (req, res) => {
     symmetry,
     fluorescence,
   });
+  logger.info(`Price calculated :${price.price}`);
   res.status(200).json(price);
 });
 
